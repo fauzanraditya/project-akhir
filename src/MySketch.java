a462c6505bfe82431e5037ec668642457515d384
fauzanraditya <raditwildan1@gmail.com>
2023-12-01 13:51:26 +0700

    LAST FINAL COMMIT

--- src/MySketch.java
+++ src/MySketch.java
@@ -5,14 +5,24 @@
  * Sistem Informasi - E
  */
 
-
 import processing.core.PApplet;
+import ddf.minim.*;
 
 public class MySketch extends PApplet {
 
+    // Inisiasi variabel
     float ye = 90;
     float xe = 0;
+    float xAwan = 0;
     boolean siang = true;
+    float lalatX, lalatY; // Koordinat lalat
+    boolean terbang = false;
+    Minim minim;
+    AudioPlayer ngueng;
+    int x1 = 20;
+    int x2 = 128;
+    int x3 = 45;
+    int x4 = 105;
 
     public static void main(String[] args) {
         MySketch mySketch = new MySketch();
@@ -21,41 +31,55 @@ public class MySketch extends PApplet {
 
     @Override
     public void settings() {
-        size(808, 550);
+        size(1200, 700);
+        // Import sound
+        minim = new Minim(this);
+        ngueng = minim.loadFile("/assets/ngueng.mp3");
 
+    }
+
+    @Override
+    public void setup() {
+        // Inisasi koordinat lalat
+        lalatX = width / 2;
+        lalatY = height / 2;
     }
 
     @Override
     public void draw() {
 
+        // Perubahan langit
+
+        // Siang
         if (siang == true) {
             background(25, 25, 255);
             langitSiang();
-            // background(78,155,17);
             fill(255, 255, 0);
-            ellipse(xe += 10, ye, 75, 75);
-            if (xe > 725) {
+            ellipse(xe += 1, ye, 75, 75);
+            if (xe > 1250) {
                 siang = false;
                 xe = 0;
             }
+            // Malam
         } else if (siang == false) {
             background(25, 25, 112);
             langitMalam();
             fill(128, 128, 128);
-            ellipse(xe += 10, ye, 75, 75);
-            if (xe > 725) {
+            ellipse(xe += 1, ye, 75, 75);
+            if (xe > 1250) {
                 siang = true;
                 xe = 0;
             }
         }
-        delay(500);
+
+        // Memanggil method
         gunung();
         jalan();
         lahan();
         rumah();
         mobil();
         hujan();
-        awan();
+        awan(xAwan);
         dinding();
         lantai();
         atap();
@@ -66,54 +90,116 @@ public class MySketch extends PApplet {
         cpuKomputer();
         lampu();
         jam();
+        drawLalat(lalatX, lalatY);
+        lalatTerbang();
+
+        // Perubahan posisi lalat
+        if (mousePressed) {
+            // Mengikuti pergerakan mouse
+            float targetX = mouseX;
+            float targetY = mouseY;
+
+            // Menghitung perubahan posisi
+            float deltaX = targetX - lalatX;
+            float deltaY = targetY - lalatY;
+
+            // Menggerakkan lalat mendekati posisi mouse
+            lalatX += deltaX * 0.05;
+            lalatY += deltaY * 0.05;
+        }
+
+    }
+
+    void lalatTerbang() {
+        if (mousePressed) {
+            terbang = true;
+        } else {
+            terbang = false;
+        }
+
+        if (terbang == true) {
+            ngueng.play();
+
+            // Mengikuti pergerakan mouse
+            float targetX = mouseX;
+            float targetY = mouseY;
+
+            // Menghitung perubahan posisi
+            float deltaX = targetX - lalatX;
+            float deltaY = targetY - lalatY;
+
+            // Menggerakkan lalat mendekati posisi mouse
+            lalatX += deltaX * 0.05;
+            lalatY += deltaY * 0.05;
+        } else if (terbang == false) {
+            ngueng.pause();
+            ngueng.rewind();
+        }
+    }
+
+    void drawLalat(float x, float y) {
+        // Badan
+        fill(0, 0, 255);
+        ellipse(x, y, 30, 15);
+
+        // Kepala
+        fill(0, 0, 255);
+        ellipse(x + 15, y, 10, 10);
+
+        // Sayap
+        fill(0, 0, 255, 150);
+        beginShape();
+        vertex(x + 10, y);
+        vertex(x - 5, y + 10);
+        vertex(x - 5, y - 10);
+        endShape(CLOSE);
     }
 
     public void langitSiang() {
         noStroke();
         fill(30, 30, 250);
-        rect(10, 150, 500, 10);
+        rect(10, 150, 1200, 10);
         fill(40, 40, 255);
-        rect(10, 160, 500, 10);
+        rect(10, 160, 1200, 10);
         fill(50, 50, 255);
-        rect(10, 170, 500, 10);
+        rect(10, 170, 1200, 10);
         fill(60, 60, 255);
-        rect(10, 180, 500, 10);
+        rect(10, 180, 1200, 10);
         fill(70, 70, 255);
-        rect(10, 190, 500, 10);
+        rect(10, 190, 1200, 10);
         fill(80, 80, 255);
-        rect(10, 200, 500, 10);
+        rect(10, 200, 1200, 10);
         fill(90, 90, 255);
-        rect(10, 210, 500, 10);
+        rect(10, 210, 1200, 10);
         fill(100, 100, 255);
-        rect(10, 220, 500, 10);
+        rect(10, 220, 1200, 10);
         fill(110, 110, 255);
-        rect(10, 230, 500, 10);
+        rect(10, 230, 1200, 10);
     }
 
     public void langitMalam() {
         noStroke();
         fill(25, 25, 120);
-        rect(10, 150, 500, 10);
+        rect(10, 150, 1200, 10);
         fill(25, 25, 125);
-        rect(10, 160, 500, 10);
+        rect(10, 160, 1200, 10);
         fill(25, 25, 135);
-        rect(10, 170, 500, 10);
+        rect(10, 170, 1200, 10);
         fill(25, 25, 145);
-        rect(10, 180, 500, 10);
+        rect(10, 180, 1200, 10);
         fill(25, 25, 155);
-        rect(10, 190, 500, 10);
+        rect(10, 190, 1200, 10);
         fill(25, 25, 165);
-        rect(10, 200, 500, 10);
+        rect(10, 200, 1200, 10);
         fill(25, 25, 175);
-        rect(10, 210, 500, 10);
+        rect(10, 210, 1200, 10);
         fill(25, 25, 185);
-        rect(10, 220, 500, 10);
+        rect(10, 220, 1200, 10);
         fill(25, 25, 195);
-        rect(10, 230, 500, 10);
+        rect(10, 230, 1200, 10);
     }
 
     public void gunung() {
-        // Gunung
 
         strokeWeight(3);
         fill(0, 94, 0);
@@ -124,10 +210,9 @@ public class MySketch extends PApplet {
     }
 
     public void jalan() {
-        // Jalan
         noStroke();
         fill(0);
-        quad(0, 380, 380, 380, 380, 440, 0, 440);
+        quad(0, 380, 1200, 380, 1200, 440, 0, 440);
         strokeWeight(4);
         stroke(255);
         line(20, 405, 48, 405);
@@ -141,23 +226,42 @@ public class MySketch extends PApplet {
         line(340, 405, 368, 405);
         line(380, 405, 408, 405);
         line(420, 405, 448, 405);
+        line(460, 405, 488, 405);
+        line(500, 405, 528, 405);
+        line(540, 405, 568, 405);
+        line(580, 405, 608, 405);
+        line(620, 405, 648, 405);
+        line(660, 405, 688, 405);
+        line(700, 405, 728, 405);
+        line(740, 405, 768, 405);
+        line(780, 405, 808, 405);
+        line(820, 405, 848, 405);
+        line(860, 405, 888, 405);
+        line(900, 405, 928, 405);
+        line(940, 405, 968, 405);
+        line(980, 405, 1008, 405);
+        line(1020, 405, 1048, 405);
+        line(1060, 405, 1088, 405);
+        line(1100, 405, 1128, 405);
+        line(1140, 405, 1168, 405);
+        line(1180, 405, 1208, 405);
     }
 
     public void lahan() {
-        // Lahan
         noStroke();
         fill(80, 49, 33);
-        quad(0, 330, 388, 330, 380, 380, 0, 380);
+        quad(0, 330, 1200, 330, 1200, 380, 0, 380);
     }
 
     public void rumah() {
-        // Rumah
         noStroke();
+        // Kiri
         fill(125, 87, 46);
         triangle(55, 323, 23, 345, 87, 345);
         fill(182, 181, 216);
         rect(30, 345, 48, 28);
 
+        // Kanan
         fill(125, 87, 46);
         triangle(120, 320, 90, 340, 150, 340);
         fill(255, 255, 0);
@@ -165,24 +269,26 @@ public class MySketch extends PApplet {
     }
 
     public void mobil() {
-        int second = second();
-
-        // Mobil
+        if (x1 > 1200) {
+            x1 = 20;
+            x2 = 128;
+            x3 = 45;
+            x4 = 105;
+        }
+        float random1 = random(1, 10);
         fill(104, 105, 188);
-        rect(20 + 10 * second, 350, 108, 58);
-        // fill(124, 282, 8);
-        // rect(128 + 10 * second, 265, 48, 35);
+        rect(x1 += random1, 350, 108, 58);
         fill(94, 85, 188);
-        rect(128 + 10 * second, 373, 20, 35);
+        rect(x2 += random1, 373, 20, 35);
         fill(65);
-        ellipse(45 + 10 * second, 400, 30, 30);
-        ellipse(105 + 10 * second, 400, 30, 30);
+        ellipse(x3 += random1, 400, 30, 30);
+        ellipse(x4 += random1, 400, 30, 30);
+
     }
 
     public void hujan() {
         int y = 10;
 
-        // Hujan
         if (y > 1000) {
             y = 10;
         }
@@ -196,46 +302,58 @@ public class MySketch extends PApplet {
     }
 
     public void dinding() {
-        // Dinding
         strokeWeight(8);
         noStroke();
         fill(168, 208, 218);
+
+        // Kiri
         quad(0, 8, 480, 8, 480, 40, 0, 48);
-        quad(370, 0, 808, 0, 808, 450, 370, 450);
+        quad(370, 0, 818, 0, 818, 450, 370, 450);
         quad(0, 440, 389, 440, 380, 458, 0, 458);
         quad(0, 0, 0, 450, 20, 450, 20, 0);
+
+        // Kanan
+        quad(1190, 20, 1200, 20, 1200, 450, 1190, 450);
+        quad(818, 440, 1200, 440, 1200, 458, 818, 458);
+        quad(818, 20, 1200, 20, 1200, 40, 818, 40);
     }
 
     public void lantai() {
-        // lantai
         fill(250, 250, 200);
-        quad(0, 450, 820, 450, 820, 550, 0, 550);
+        quad(0, 450, 1220, 450, 1220, 700, 0, 700);
         stroke(0);
         strokeWeight(3);
-        line(0, 450, 820, 450);
-        line(0, 500, 820, 500);
-        line(10, 450, -50, 550);
-        line(100, 450, 50, 550);
-        line(200, 450, 150, 550);
-        line(300, 450, 250, 550);
-        line(400, 450, 350, 550);
-        line(500, 450, 450, 550);
-        line(600, 450, 550, 550);
-        line(700, 450, 650, 550);
-        line(800, 450, 750, 550);
+        line(0, 450, 1220, 450);
+        line(0, 500, 1220, 500);
+        line(0, 550, 1220, 550);
+        line(0, 600, 1220, 600);
+        line(0, 650, 1220, 650);
+        line(0, 700, 1220, 700);
+        line(10, 450, -50, 700);
+        line(100, 450, 50, 700);
+        line(200, 450, 150, 700);
+        line(300, 450, 250, 700);
+        line(400, 450, 350, 700);
+        line(500, 450, 450, 700);
+        line(600, 450, 550, 700);
+        line(700, 450, 650, 700);
+        line(800, 450, 750, 700);
+        line(900, 450, 850, 700);
+        line(1000, 450, 950, 700);
+        line(1100, 450, 1050, 700);
+        line(1200, 450, 1150, 700);
     }
 
     public void atap() {
-        // list
         stroke(0);
         strokeWeight(2);
         fill(255);
-        quad(0, 0, 820, 0, 820, 30, 0, 30);
-        line(0, 25, 820, 25);
+        quad(0, 0, 1220, 0, 1220, 30, 0, 30);
+        line(0, 25, 1220, 25);
     }
 
     public void jendela() {
-        // jendela
+        // Kiri
         strokeWeight(0);
         fill(189, 138, 63);
         quad(10, 40, 380, 40, 380, 70, 10, 70);
@@ -246,16 +364,25 @@ public class MySketch extends PApplet {
         quad(10, 40, 20, 40, 20, 440, 10, 440);
         quad(370, 40, 380, 40, 380, 440, 370, 440);
         quad(190, 40, 200, 40, 200, 440, 190, 440);
+
+        // Kanan
+        strokeWeight(0);
+        quad(820, 40, 1190, 40, 1190, 70, 820, 70);
+        quad(820, 440, 1190, 440, 1190, 430, 820, 430);
+        strokeWeight(4);
+        line(820, 50, 1190, 50);
+        line(820, 60, 1190, 60);
+        quad(820, 40, 830, 40, 830, 440, 820, 440);
+        quad(1180, 40, 1190, 40, 1190, 440, 1180, 440);
+        quad(1000, 40, 1010, 40, 1010, 440, 1000, 440);
     }
 
     public void meja() {
-        // meja
         strokeWeight(1);
         fill(159, 89, 0);
         quad(430, 305, 780, 305, 780, 320, 430, 320);
         rect(550, 320, 230, 100);
         rect(440, 320, 130, 129);
-        // quad(780, 300, 440, 300, 440, 450, 430, 450);
         quad(780, 300, 790, 300, 790, 450, 780, 450);
         strokeWeight(1);
         line(665, 320, 665, 420);
@@ -267,7 +394,6 @@ public class MySketch extends PApplet {
     }
 
     public void buku() {
-        // buku
         strokeWeight(0);
         fill(255, 255, 255);
         quad(690, 290, 770, 290, 770, 305, 690, 305);
@@ -295,8 +421,6 @@ public class MySketch extends PApplet {
     }
 
     public void komputer() {
-        // komputer
-        // monitor komputer
         strokeWeight(5);
         stroke(192, 192, 192);
         fill(0);
@@ -317,7 +441,6 @@ public class MySketch extends PApplet {
     public void cpuKomputer() {
         int second = second();
 
-        // cpu komputer
         stroke(0);
         line(455, 355, 495, 355);
         line(455, 365, 495, 365);
@@ -335,7 +458,6 @@ public class MySketch extends PApplet {
     }
 
     public void lampu() {
-        // lampu
         fill(250, 240, 230);
         quad(631, 240, 639, 240, 639, 300, 631, 300);
         quad(615, 298, 655, 298, 655, 305, 615, 305);
@@ -351,7 +473,6 @@ public class MySketch extends PApplet {
         int second = second();
         int minute = minute();
         int hour = hour();
-        // jam
         strokeWeight(3);
         stroke(0);
         fill(249, 19, 147);
@@ -369,19 +490,22 @@ public class MySketch extends PApplet {
         line(600, 90, cos(hour) * 20 + 600, sin(hour) * 20 + 90);
     }
 
-    public void awan() {
-        int second = second();
-        // Awan
+    public void awan(float x) {
         stroke(0, 0, 0);
         strokeWeight(2);
         fill(95, 158, 160);
-        ellipse(-70 + 8 * second, 150, 30, 15);
-        ellipse(-40 + 8 * second, 150, 45, 35);
-        ellipse(-10 + 8 * second, 150, 30, 15);
+        ellipse(-70 + x, 150, 30, 15);
+        ellipse(-40 + x, 150, 45, 35);
+        ellipse(-10 + x, 150, 30, 15);
 
         fill(95, 158, 160);
-        ellipse(-50 + 8 * second, 120, 30, 10);
-        ellipse(-10 + 8 * second, 120, 40, 30);
-        ellipse(10 + 8 * second, 120, 28, 18);
+        ellipse(-50 + x, 120, 30, 10);
+        ellipse(-10 + x, 120, 40, 30);
+        ellipse(10 + x, 120, 28, 18);
+
+        xAwan += 2;
+        if (x > width + 50) {
+            xAwan = -700; // Jika awan mencapai tepi kanan, kembali ke posisi awal
+        }
     }
 }
